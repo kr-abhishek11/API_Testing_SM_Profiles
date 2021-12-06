be46711d6374fa069f4e331abe8c3a24ddd00a90
Kumar Abhishek <73335474+kr-abhishek11@users.noreply.github.com>
2021-12-06 19:29:19 +0530

    Update test_api_calls.py

--- test_api_calls.py
+++ test_api_calls.py
@@ -15,11 +15,8 @@ def test_get():
     url_get = config['URL']['get']
     resp = requests.get(url_get)
     resp_data = resp.json()
-    #print(resp_data)
-    # list_get_data = list(resp_data.items())
-    # print(list_get_data[0])
-    print("**********************")
-    print("GET URL ",url_get)
+    print("**********  GET URL  ************")
+    print(url_get)
     print('\n')
     print(resp_data)
     print("**********************")
@@ -83,11 +80,6 @@ def test_put():
     put_data = resp_after_put.json()
     '''Reversing the list so that comparison is made on the first index itself'''
     put_data.reverse()
-    # print(resp_data)
-    # print("**********************")
-    # print('\n')
-    # print(put_data[0])
-    print(resp_data)
     if resp_data == put_data[0]:
         print(resp_data)
         print('\n')
@@ -103,35 +95,19 @@ def test_delete():
     ''' This function is used to test deletion of bandwidth data'''
     #url = 'http://127.0.0.1:5000/bwtier/1'
     url_delete = config['URL']['delete']
-   
-    #url = BASE_URL + '/bwtier_id'
-    data = json.dumps(bwtier_api.BW_TIER_DATA)
-    headers = {'Accept':'application/json','Content-Type':'application/json'}
     resp = requests.delete(url_delete)
     resp_data = resp.json()
     print("**********************")
     print(url_delete)
     print('\n')
-    print(resp_data)
+    #print(resp_data)
+    # url_get = config['URL']['get']
+    # resp_after_delete = requests.get(url_get)
+    # data_after_delete = resp_after_delete.json()
+    #print(data_after_delete)
+    print("Requested data has been deleted")
     print("**********************")
     print('')
-    url_get = config['URL']['get']
-    resp_after_delete = requests.get(url_get)
-    delete_data = resp_after_delete.json()
-    
-    '''Reversing the list so that comparison is made on the first index itself'''
-    delete_data.reverse()
-    #print (resp_data)
-    print(delete_data)
-    if resp_data != delete_data[0]:
-        assert True
-        # print(delete_data[0])
-        # print('\n')
-        # print('****************NOT FOUND IN*************')
-        # print('\n')
-        # print(resp_data)
-    else:
-        assert False
-    
-
+    print(resp_data)
     assert resp.status_code == 200,"Deletion of Bandwidth tier failed"
+    
