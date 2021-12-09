d7e9a1d8a0220ac2d56f70042ca6626b805433e3
Kumar Abhishek <73335474+kr-abhishek11@users.noreply.github.com>
2021-12-09 19:10:44 +0530

    Add files via upload

---
+++ bwtier_api.py
@@ -0,0 +1,89 @@
+from flask import Flask
+from flask import json
+from flask.json import jsonify
+import pytest
+import requests
+
+BW_TIER_DATA = [{
+    "bwtier_id":0,
+    "name":"DATA_1000M_BW_TEST",
+    "downstreamPir":400000,
+    "downstreamCir":200000,
+    "upstreamCir":200000,
+    "upstreamPir":400000
+},
+{
+    "bwtier_id":1,
+    "name":"DATA_2000M_BW_TEST",
+    "downstreamPir":400000,
+    "downstreamCir":200000,
+    "upstreamCir":200000,
+    "upstreamPir":400000
+},
+{
+    "bwtier_id":2,
+    "name":"DATA_3000M_BW_TEST",
+    "downstreamPir":400000,
+    "downstreamCir":200000,
+    "upstreamCir":200000,
+    "upstreamPir":400000
+}]
+
+app = Flask(__name__)    
+        
+
+@app.route('/')
+def test_home_bwtier():
+    return "This API is created for BW Tier profile testing"
+
+@app.route('/bwtier/',methods=['GET'])
+def get():
+    #bwtier_data = open("bwtier_data.json","r").read()
+    #data = json.loads(BW_TIER_DATA)
+    #return jsonify({"BW TIER":BW_TIER_DATA})
+    return json.dumps(BW_TIER_DATA)
+    #return jsonify({BW_TIER_DATA})
+
+# GET API call for fetching all BW_TIER data
+@app.route('/bwtier/<int:bwtier_id>',methods=['GET'])
+def get_bwtier(bwtier_id):
+    # url = "http://localhost/5000/bwtier/"
+    # resp = requests.get(url)
+    # assert resp.status_code==200,"Response code does not match"
+    return jsonify({'id':BW_TIER_DATA[bwtier_id]})
+
+# POST API Call for creating a new BW_TIER_DATA
+@app.route('/bwtier/create',methods=['POST'])
+def create():
+    new_bw_tier =[{
+        "bwtier_id":"3",
+        "name":"DATA_4000M_BW_TEST",
+        "downstreamPir":400000,
+        "downstreamCir":200000,
+        "upstreamCir":200000,
+        "upstreamPir":400000
+    }]
+    BW_TIER_DATA.append(new_bw_tier)
+    #return jsonify({'BW TIER': new_bw_tier})
+    return json.dumps(new_bw_tier)
+
+# PUT API Call to update an existing BW_TIER_DATA
+@app.route("/bwtier/update/<int:bwtier_id>",methods=['PUT'])
+def bwtier_update(bwtier_id):
+     BW_TIER_DATA[bwtier_id]['name']="XYZ"
+     BW_TIER_DATA.append(BW_TIER_DATA[bwtier_id]) #updating the list after appending
+     return json.dumps(BW_TIER_DATA[bwtier_id])
+
+
+# DELETE API call to delete an existing BW_TIER_DATA
+@app.route("/bwtier/delete/<int:bwtier_id>",methods= ['DELETE'])
+def bwtier_delete(bwtier_id):
+    #BW_TIER_DATA.remove(BW_TIER_DATA[bwtier_id])
+    if(BW_TIER_DATA[bwtier_id] in BW_TIER_DATA):
+        BW_TIER_DATA.remove(BW_TIER_DATA[bwtier_id])
+    return json.dumps(BW_TIER_DATA)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ db_post_validate.py
@@ -0,0 +1,71 @@
+import psycopg2
+import psycopg2.extras
+import requests
+from bwtier_api import BW_TIER_DATA
+from test_api_calls import *
+from configparser import ConfigParser
+file = "config.ini"
+config = ConfigParser()
+config.read(file)
+
+
+hostname = config['DATABASE']['HOST']
+database = config['DATABASE']['DB']
+username = config['DATABASE']['USERNAME']
+pwd = config['DATABASE']['PASSWORD']
+port_id = config['DATABASE']['PORT']
+
+
+
+conn= None
+cur= None
+try:
+    with psycopg2.connect(
+                host = hostname,
+                dbname = database,
+                user = username,
+                password = pwd,
+                port = port_id ) as conn:
+    
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+            # Saving POST value in GET table itself
+            url_post = config['URL']['post']
+            resp = requests.post(url_post)
+            resp_data = resp.json() 
+            for i in range(len(resp_data)):
+                bw_tier_data = resp_data[i]
+                bw_tier_data_list = []
+                for key,value in bw_tier_data.items():
+                     bw_tier_data_list.append(value)
+                record_to_insert = bw_tier_data_list        
+                insert_script = '''INSERT INTO bwtier_data(bwtier_id,downstreamPir,downstreamCir,name,upstreamPir,upstreamCir) VALUES(%s,%s,%s,%s,%s,%s) 
+                                          '''
+                cur.execute(insert_script,record_to_insert)
+                conn.commit()
+
+
+            # this code snippet is used to validate the database data after the POST API call is done   
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                cur.execute("SELECT * from bwtier_data")
+                rows = cur.fetchall() #fetching all the rows from our DB
+                print(rows[-1]) #fetching the last row from DB after GET API call insertion
+                print('\n')
+                print('**********MATCHES*************')
+                print('\n')
+                print(record_to_insert) #this variable holds the last row from our GET API call
+                if record_to_insert == rows[-1]:
+                    print("API data is present in our DB")
+                else:
+                    print("API data is missing from DB")
+
+            
+           
+
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
